41f3dc250e32b6c9d64fe954f0980b9c7e44987d
Sayesha140 <enaya871@gmail.com>
2024-11-20 16:08:08 +0600

    3rd

--- src/Order.java
+++ src/Order.java
@@ -1,2 +1,20 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Order {
+    List<Flavor>flavors;
+    List<Topping>toppings;
+
+    public Order(List<Flavor>flavors,List<Topping>toppings){
+        this.flavors=new ArrayList<>();
+        this.toppings=new ArrayList<>();
+    }
+
+    public void addFlavors(Flavor flavor){
+        flavors.add(flavor);
+    }
+
+    public void addToppings(Topping topping){
+        toppings.add(topping);
+    }
 }
